1c4844a9ca5c31dca568bbd566e369a0a0975b04
fisher_998 <102145839@qq.com>
2019-06-13 14:48:34 +0800

    修改版本号1.1.3

--- siteinfo.js
+++ siteinfo.js
@@ -1,7 +1,7 @@
 export default {
   "uniacid": "999",//小程序uniacid
   "multiid": "0",  //小程序版本id
-  "version": "1.0",  //小程序版本号
-  "siteroot": "https://longbing.chengyouhudong.com/index.php",
-  // "siteroot": "http://longbing.chengyouhudong.com/index.php", // 开发
+  "version": "1.1.3",  //小程序版本号
+  // "siteroot": "https://longbing.chengyouhudong.com/index.php",
+  "siteroot": "http://longbing.chengyouhudong.com/index.php", // 开发
 };
